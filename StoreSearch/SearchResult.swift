1eea7f4d040cac4360a0e915b73d0d1d8772de93
Mustafa Paslı <mustafapasli94@gmai.com>
2020-10-26 22:46:18 +0300

    Add a search bar and table view

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Mustafa Paslı on 26.10.2020.
+//  Copyright © 2020 Mustafa Paslı. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
