00c5965c47787e33d23faccace23d9f861e90d24
Mustafa Paslı <mustafapasli94@gmai.com>
2020-10-26 17:16:36 +0300

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Mustafa Paslı on 26.10.2020.
+//  Copyright © 2020 Mustafa Paslı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
